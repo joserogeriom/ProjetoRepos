2410fa04fe16a1c82979129f4e61c6ed5bb6f9d0
joserogeriom <joserogerio_@hotmail.com>
2019-07-26 22:40:01 -0300

    Adicionar arquivos de projeto.

---
+++ TesteRepositorio/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TesteRepositorio
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
